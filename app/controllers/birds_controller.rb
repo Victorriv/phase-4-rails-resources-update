9511714b70651dddb74a05ea78154e261360a508
Victor <victorrivera714@yahoo.com>
2021-12-17 09:20:09 -0800

    commit tree 260e75e937408508199e39d66f71bef9e5ecc68d

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -22,10 +22,32 @@ class BirdsController < ApplicationController
     end
   end
 
+   # PATCH /birds/:id
+   def update
+    #find the bird we're trying to update
+    bird = Bird.find_by(id: params[:id])
+    #update the bird, using data from body
+    bird.update(birds_params)
+    #send a response with the updated bird
+    render json: bird
+    
+  end
+
+  def increment_likes
+    bird= Bird.find_by(id: params[:id])
+    if bird 
+      bird.updated(likes: bird.likes + 1)
+      render json: bird
+    else 
+      render json: {error: "Bird no found"}, status: :not_found
+    end
+
+  end
+
   private
 
   def bird_params
-    params.permit(:name, :species)
+    params.permit(:name, :species, :likes)
   end
 
 end
